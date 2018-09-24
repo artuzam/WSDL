4c1c81e97db38e17af92296872640ba7589096f8
artuzam <charliecazc@gmail.com>
2018-09-23 19:07:14 -0600

    Se añadió el header

--- createWSDL/src/com/company/Main.java
+++ createWSDL/src/com/company/Main.java
@@ -5,4 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static String getWSDL(){
+        String WSDL = "";
+
+        String header = "<?xml version=\"1.0\" ?>\n" + "\"<definitions name=\\\"$this->service_name\\\" targetNamespace=\\\"urn:$this->service_name\\\" xmlns:wsdl=\\\"http://schemas.xmlsoap.org/wsdl/\\\" xmlns:soap=\\\"http://schemas.xmlsoap.org/wsdl/soap/\\\" xmlns:tns=\\\"urn:$this->service_name\\\" xmlns:xsd=\\\"http://www.w3.org/2001/XMLSchema\\\" xmlns:SOAP-ENC=\\\"http://schemas.xmlsoap.org/soap/encoding/\\\" xmlns=\\\"http://schemas.xmlsoap.org/wsdl/\\\">\\n\"" + "<types xmlns=\"http://schemas.xmlsoap.org/wsdl/\" />\n";
+
+
+
+
+        return WSDL;
+    }
 }
