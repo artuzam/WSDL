4abc23e89d9fdffcbebc592d09911dcc46984bc3
Daniel González <daa.gonzalez@gmail.com>
2018-09-25 14:55:33 -0600

    Se agregaron métodos para ver parametros y nombres de la clase.

---
+++ createWSDL/src/com/company/HolaMundo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class HolaMundo {
+    public String salude(String nombre) {
+        return "Hola " + nombre;
+    }
+
+    public int sumar(int a, int b) {
+        return  a+b;
+    }
+}
--- createWSDL/src/com/company/Main.java
+++ createWSDL/src/com/company/Main.java
@@ -1,15 +1,39 @@
 package com.company;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        getWSDL("HolaMundo");
     }
 
-    public static String getWSDL(){
+    public static String getWSDL(String serviceName){
         String WSDL = "";
 
-        String header = "<?xml version=\"1.0\" ?>\n" + "\"<definitions name=\\\"$this->service_name\\\" targetNamespace=\\\"urn:$this->service_name\\\" xmlns:wsdl=\\\"http://schemas.xmlsoap.org/wsdl/\\\" xmlns:soap=\\\"http://schemas.xmlsoap.org/wsdl/soap/\\\" xmlns:tns=\\\"urn:$this->service_name\\\" xmlns:xsd=\\\"http://www.w3.org/2001/XMLSchema\\\" xmlns:SOAP-ENC=\\\"http://schemas.xmlsoap.org/soap/encoding/\\\" xmlns=\\\"http://schemas.xmlsoap.org/wsdl/\\\">\\n\"" + "<types xmlns=\"http://schemas.xmlsoap.org/wsdl/\" />\n";
+        String header = "<?xml version=\"1.0\" ?>\n" + "\"<definitions name=\\\"" + serviceName + "\\\" targetNamespace=\\\"urn:" + serviceName + "\\\" xmlns:wsdl=\\\"http://schemas.xmlsoap.org/wsdl/\\\" xmlns:soap=\\\"http://schemas.xmlsoap.org/wsdl/soap/\\\" xmlns:tns=\\\"urn:" + serviceName + "\\\" xmlns:xsd=\\\"http://www.w3.org/2001/XMLSchema\\\" xmlns:SOAP-ENC=\\\"http://schemas.xmlsoap.org/soap/encoding/\\\" xmlns=\\\"http://schemas.xmlsoap.org/wsdl/\\\">\\n\"" + "<types xmlns=\"http://schemas.xmlsoap.org/wsdl/\" />\n";
+
+        ClassLoader classLoader = Main.class.getClassLoader();
+
+        try {
+            Class aClass = classLoader.loadClass("com.company.HolaMundo");
+            Method[] metodos = aClass.getDeclaredMethods();
+            for (Method metodo:
+                 metodos) {
+                System.out.println("Nombre del método: " + metodo.getName() + " - Retorna: " + metodo.getReturnType().getSimpleName());
+                Parameter[] parametros = metodo.getParameters();
+                System.out.println(parametros.length + " Parámetros: ");
+                for (Parameter parametro :
+                        parametros) {
+                    System.out.println(parametro.getName() + " - " + parametro.getType().getSimpleName());
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
 
 
