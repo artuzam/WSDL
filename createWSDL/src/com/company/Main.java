091a5d83d57d13de71a3fd4610c8bc3f79639ade
artuzam <charliecazc@gmail.com>
2018-09-25 23:45:33 -0600

    Se añadió typeMedio y el array tipos para verificar si es un tipo primitivo

--- createWSDL/src/com/company/Main.java
+++ createWSDL/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -13,30 +15,32 @@ public class Main {
     public static String getWSDL(String serviceName){
         String WSDL = "";
         String types; //types = typesInicio + typesMedio + typesFinal
+        List<String> tipos = Arrays.asList("String", "boolean", "int", "integer", "float", "double"); //para verifcar si es un tipo primitivo
 
-        String header = "<?xml version=\"1.0\" ?>\n" + "<definitions name=\"" + serviceName + "\" targetNamespace=\"urn:" + serviceName + "\"\n xmlns:wsdl=\"http://schemas.xmlsoap.org/wsdl/\"\n xmlns:soap=\"http://schemas.xmlsoap.org/wsdl/soap/\"\n xmlns:tns=\"urn:" + serviceName + "\"\n xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\"\n xmlns=\"http://schemas.xmlsoap.org/wsdl/\">\n" + "<types xmlns=\"http://schemas.xmlsoap.org/wsdl/\" />\n";
+        String header = "<?xml version=\"1.0\" ?>\n" + "<definitions name=\"" + serviceName + "\" targetNamespace=\"urn:" + serviceName + "\"\n xmlns:wsdl=\"http://schemas.xmlsoap.org/wsdl/\"\n xmlns:soap=\"http://schemas.xmlsoap.org/wsdl/soap/\"\n xmlns:tns=\"urn:" + serviceName + "\"\n xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\"\n xmlns=\"http://schemas.xmlsoap.org/wsdl/\">\n" + "<types xmlns=\"http://schemas.xmlsoap.org/wsdl/\" />\n\n";
 
         String typesInicio = "<types>\n\t<schema>\n";
         String typesFinal = "\n\t<schema>\n</types>";
-
+        String typesMedio = "";
         ClassLoader classLoader = Main.class.getClassLoader();
 
         try {
             Class aClass = classLoader.loadClass("com.company.HolaMundo");
             Method[] metodos = aClass.getDeclaredMethods();
-            for (Method metodo:
-                 metodos) {
+            for (Method metodo : metodos) {
                 System.out.println("Nombre del método: " + metodo.getName() + " - Retorna: " + metodo.getReturnType().getSimpleName());
                 Parameter[] parametros = metodo.getParameters();
                 System.out.println(parametros.length + " Parámetros: ");
-                for (Parameter parametro :
-                        parametros) {
+                for (Parameter parametro : parametros) {
+                    typesMedio += "\t\t<element name=\"" + parametro.getName() + "\" type=\"" + parametro.getType().getSimpleName() + "/>\n";
                     System.out.println(parametro.getName() + " - " + parametro.getType().getSimpleName());
                 }
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        types = typesInicio + typesMedio + typesFinal;
+        System.out.println(header+types); //como va quedando el WSDL
 
         return WSDL;
     }
